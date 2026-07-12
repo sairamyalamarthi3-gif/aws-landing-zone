fccac797d7b5f544dfb0bdc294ec91981dc564c7
Sairam Yalamarthi <sairamyalamarthi3@gmail.com>
2026-07-12 13:09:09 +0000

    feat(platform): add networking, security and load balancer modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,28 @@ module "vpc" {
   enable_dns_hostnames = true
 }
 
+module "security_groups" {
+
+  source = "../modules/security-groups"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.vpc.vpc_id
+}
+module "alb" {
+
+  source = "../modules/alb"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.vpc.vpc_id
+
+  public_subnets = [
+    module.vpc.public_subnet_a_id,
+    module.vpc.public_subnet_b_id
+  ]
+
+  alb_security_group_id = module.security_groups.alb_security_group_id
+}
